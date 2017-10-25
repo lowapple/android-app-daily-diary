ad1aa4865a800561a50ddeef898874aee7b619c3
Youn Junseo <tapeim99@gmail.com>
2017-10-25 11:11:27 +0900

    initialize commit

---
+++ app/src/main/java/com/tapeim99/onedaydiary/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tapeim99.onedaydiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
